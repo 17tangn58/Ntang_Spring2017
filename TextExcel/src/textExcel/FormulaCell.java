cdd07e7cb607a85042a97ce44b2282a97b043d46
17tangn58 <17tangn58@millshighsch.org>
2017-03-24 09:47:45 -0700

    Started Part B

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -12,11 +12,23 @@ public class FormulaCell extends RealCell {
 	}
 	public String abbreviatedCellText(){
 		//returns answer of the formula as a string
-		return String.format("%-10s", ""+ans);
+		return String.format("%-10s", ""+getDoubleValue());
 	}
 	public double getDoubleValue(){
 		//returns the answer to the formula as a double
-		return ans;
+		String[] formula = getString().split(" ");
+		double answer=0.0;
+		for(int i=0;i<formula.length;i++){
+			if(formula[i].equals("+"))
+				answer = Double.parseDouble(formula[i-1])+Double.parseDouble(formula[i+1]);
+			else if(formula[i].equals("/"))
+				answer = Double.parseDouble(formula[i-1])/Double.parseDouble(formula[i+1]);
+			else if(formula[i].equals("*"))
+				answer = Double.parseDouble(formula[i-1])*Double.parseDouble(formula[i+1]);
+			else if(formula[i].equals("-"))
+				answer = Double.parseDouble(formula[i-1])-Double.parseDouble(formula[i+1]);
+		}
+		return answer;
 	}
 	
 }
